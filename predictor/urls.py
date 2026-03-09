fa622f21817fb0ef2a86f916f7a4c10ace68f6f6
t-r-a-c-y <tracytesi69@gmail.com>
2026-03-09 20:41:55 +0200

    new updates

--- predictor/urls.py
+++ predictor/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from predictor import views
+from django.views.generic import RedirectView  # Add this import
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/data_exploration/'), name='home'),  # Add this line for root redirect
     path("data_exploration", views.data_exploration_view, name="data_exploration"),
     path("regression_analysis", views.regression_analysis, name="regression_analysis"),
     path("classification_analysis", views.classification_analysis, name="classification_analysis"),
