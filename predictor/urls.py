d9023a7a9196145497a9e81b5128f5e16555a0c2
t-r-a-c-y <tracytesi69@gmail.com>
2026-03-09 09:11:39 +0200

    building

--- predictor/urls.py
+++ predictor/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from predictor import views
 
 urlpatterns = [
-    path("data_exploration", views.data_exploration_view, name="data_exploration"),
+    path("", views.data_exploration_view, name="data_exploration"),
+    path("regression_analysis", views.regression_analysis, name="regression_analysis"),
+    path("classification_analysis", views.classification_analysis, name="classification_analysis"),
+    path("clustering_analysis", views.clustering_analysis, name="clustering_analysis"),
 ]
-
-
