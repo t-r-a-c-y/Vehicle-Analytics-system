98ff2b51c7f95b55a5459c86b3d0d59d66a19920
t-r-a-c-y <tracytesi69@gmail.com>
2026-03-09 09:43:05 +0200

    building

--- predictor/urls.py
+++ predictor/urls.py
@@ -3,16 +3,6 @@ from predictor import views
 from . import views
 
 urlpatterns = [
-    path("", views.data_exploration_view, name="data_exploration"),
-    path("predict/", views.predict_view, name="predict"),
-    path("regression_analysis", views.regression_analysis, name="regression_analysis"),
-    path("classification_analysis", views.classification_analysis, name="classification_analysis"),
-    path("clustering_analysis", views.clustering_analysis, name="clustering_analysis"),
-]
-
-
-urlpatterns = [
-    path("", views.data_exploration_view, name="data_exploration"),
-   
+    path("data_exploration", views.data_exploration_view, name="data_exploration"),
 ]
 
